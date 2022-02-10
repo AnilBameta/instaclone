5e8cebe293fe56e7a930d026152ebb76a61409e7
AnilBameta <anilbameta@gmail.com>
2022-02-10 23:31:59 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import Home from './pages/Home'
+import Home from './pages/Home';
 import './App.css';
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <Home />
+    <Router>
+      <Routes>
+    <Route exact path='/' element= {<Register/>} />
+    <Route exact path='/login' element= {<Login/>} />
+    <Route exact path='/home' element= {<Home/>} />
+    </Routes>
+    </Router>
+
   );
 }
 
